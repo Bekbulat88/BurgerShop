fb114059f17a2893b502022742e1a42c04d0f397
Bekbulat88 <bek.bekeyev@gmail.com>
2023-02-25 02:21:37 +0600

    add_api_to_products_and_fix_catalog

--- src/components/CatalogProduct/CatalogProduct.jsx
+++ src/components/CatalogProduct/CatalogProduct.jsx
@@ -1,22 +1,21 @@
+import { API_URI, POSTFIX } from '../../const';
 import style from './CatalogProduct.module.css';
 
 export const CatalogProduct = (props) => {
   return (
     <article className={style.product}>
-      <img src="img/photo-5.jpg" alt={props.title} className={style.image} />
+      <img src={`${API_URI}/${props.elem.image}`} alt={props.elem.title} className={style.image} />
 
       <p className={style.price}>
-        689<span className="currency">₽</span>
+        {props.elem.price}
+        <span className="currency">₽</span>
       </p>
 
       <h3 className={style.title}>
-        <button className={style.detail}>
-          {/* Мясная бомба */}
-          {props.title}
-        </button>
+        <button className={style.detail}>{props.elem.title}</button>
       </h3>
 
-      <p className={style.weight}>520г</p>
+      <p className={style.weight}>{props.elem.weight}г</p>
 
       <button className={style.add} type="button">
         Добавить
